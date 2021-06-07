eef81a3ca8a1027a2c483d798f5ff2eeac744aa4
slametpolkam <slamet@poltek-kampar.ac.id>
2021-06-07 10:59:07 +0700

    latihan 1 tif 1 A 2020

---
+++ Latihan1/src/latihan1/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+
+/**
+ *
+ * @author oryza
+ */
+class Main {
+    public static void main(String [] args){
+//        Segitiga segitiga=new Segitiga();
+//        double alas=10.5,tinggi=8;
+//        System.out.println(segitiga.luas_segitiga(alas, tinggi));
+        Login log=new Login();
+        log.setVisible(true);
+    }
+}
---
+++ Latihan1/src/latihan1/Segitiga.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+
+/**
+ *
+ * @author oryza
+ */
+public class Segitiga {
+     double luas_segitiga(double alas, double tinggi){
+        double luas=(alas*tinggi)/2;
+        return luas;
+    } 
+}
